1b501d52841ec65bf93fae4ab52bbf6abfec5f91
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-02-12 15:38:00 -0300

    add phone input

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Dog, User } from 'lucide-react'
+import { Dog, Phone, User } from 'lucide-react'
 import { Controller, useForm } from 'react-hook-form'
+import { IMaskInput } from 'react-imask'
 import z from 'zod'
 import { Button } from '@/components/ui/button'
 import {
@@ -138,12 +139,20 @@ export function AppointmentForm() {
                   >
                     Telefone
                   </FieldLabel>
-                  <Input
-                    {...field}
-                    id={field.name}
-                    aria-invalid={fieldState.invalid}
-                    placeholder="(00) 00000-0000"
-                  />
+                  <div className="relative">
+                    <Phone
+                      className="absolute left-3 top-1/2 -translate-y-1/2 transform text-content-brand"
+                      size={20}
+                    />
+                    <IMaskInput
+                      mask="(00) 0 0000-0000"
+                      className="pl-10 flex h-12 w-full rounded-md border border-border-primary bg-background-tertiary px-3 py-2 text-sm text-content-primary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-content-secondary focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-0 focus-visible:ring-border-brand disabled:cursor-not-allowed disabled:opacity-50 hover:border-border-secondary focus:border-border-brand focus-visible:border-border-brand aria-invalid:ring-destructive/20 aria-invalid:border-destructive"
+                      {...field}
+                      id={field.name}
+                      aria-invalid={fieldState.invalid}
+                      placeholder="(00) 0 0000-0000"
+                    />
+                  </div>
                   {fieldState.invalid && (
                     <FieldError errors={[fieldState.error]} />
                   )}
