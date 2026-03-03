67461f667521b57b595509bafd117e463c38fa8d
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-03-03 08:49:54 -0300

    add feedback menssage with toast

--- src/components/appointment-form/appointment-form.tsx
+++ src/components/appointment-form/appointment-form.tsx
@@ -13,6 +13,7 @@ import {
 } from 'lucide-react'
 import { Controller, useForm } from 'react-hook-form'
 import { IMaskInput } from 'react-imask'
+import { toast } from 'sonner'
 import z from 'zod'
 import { Button } from '@/components/ui/button'
 import {
@@ -89,6 +90,13 @@ export function AppointmentForm() {
   })
 
   function onSubmit(data: AppointFormValues) {
+    const [hour, minute] = data.time.split(':')
+
+    const scheduleAt = new Date(data.scheduleAt)
+    scheduleAt.setHours(Number(hour), Number(minute), 0, 0)
+
+    toast.success(`Agendamento criado com sucesso!`)
+
     console.log(data)
   }
 
